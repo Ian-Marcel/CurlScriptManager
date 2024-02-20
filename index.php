2e131e680a162910833588ffc3ac207e8e11f962
fox <ianmarcel@mailfence.com>
2024-02-20 14:09:57 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="http://localhost/.elephpant.png" image/x-icon">
+        <link rel="stylesheet" href="style.css">
+        <title>php</title>
+    </head>
+    <body class="Blood">
+        <h1>Hello World!</h1>
+        <p>Exemplo de Página PHP com HTML</p>
+
+        <?php
+        $mensagem = "variable";
+        echo "<p>$mensagem</p>"
+        ?>
+
+        <!-- scripts -->
+        <script src="script.js"></script>
+
+    </body>
+</html>

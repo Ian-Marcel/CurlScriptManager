4a967761e9794a0b7057c250feb6c649979b2187
Ian <ianmarcel@mailfence.com>
2024-06-18 17:09:50 -0300

    pasta scripts criada dentro de assets, script dos botões movido para pasta assets/scripts

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,7 +21,7 @@
             </iframe>
         </main>
         <!-- scripts -->
-        <script src="/script.js"></script>
+        <script src="/assets/scripts/appButton>appDisplay.js"></script>
 
     </body>
 </html>

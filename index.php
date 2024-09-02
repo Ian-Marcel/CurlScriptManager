be2fd9e8fcb25f4e6a7765b86928b5afae6c1edb
Your Name <you@example.com>
2024-09-02 12:18:42 -0300

    footer da landing page melhorado

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/x-icon" href="assets/design/images/favicon.ico">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
         <title>Toolpot</title>
     </head>
@@ -24,8 +25,11 @@
             <img class="show" src="assets/design/images/show.png" alt="show">
         </main>
             
-        <footer>
-            <a href="https://github.com/Ian-Marcel/ToolPot">Visite nosso GitHub!</a>
+        <footer class="Sky50">
+            <span class="material-symbols-rounded">account_tree</span>
+            <a href="https://github.com/Ian-Marcel/ToolPot">
+                <span>Código fonte</span>
+            </a>
         </footer>
         <!-- scripts -->
         <script src="script.js"></script>

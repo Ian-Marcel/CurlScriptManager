58650bec78ed034466890b4556c066f3cf81a4d4
Ian <ianmarcel@mailfence.com>
2024-12-05 20:37:22 -0300

    20:37:22 - 05/12/2024

--- index.php
+++ index.php
@@ -1,37 +1,23 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" type="image/x-icon" href="assets/design/images/favicon.ico">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
-        <title>Toolpot</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚰️</text></svg>">
+        <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+        <title>Farewell</title>
     </head>
-    <body class="Navy">
-        <header class="Harvest">
-            <h3>🚧 PROJETO AINDA EM DESENVOLVIMENTO 🚧</h3>
-        </header>
-        <main>
-                <section class="tittle">
-                    <img src="assets/design/images/logo.png" alt="Logo">
-                    <h1>Toolpot</h1>
-                </section>
-            <h3>Alcance tudo sem sair do lugar</h3>
-                <section class="obtain">
-                    <a href="try-online/" class="Navy-alt">Experimente a versão online</a>
-                    <a href="#" class="Navy">Baixe a extensão</a>
-                </section>
-            <img class="show" src="assets/design/images/show.png" alt="show">
-        </main>
-            
-        <footer class="Sky50">
-            <span class="material-symbols-rounded">account_tree</span>
-            <a href="https://github.com/Ian-Marcel/ToolPot">
-                <span>Código fonte</span>
-            </a>
-        </footer>
-        <!-- scripts -->
-        <script src="script.js"></script>
+    <body>
+        <div>
+            <img class="grave_ston" src="css/assets/images/ston.png" alt="Grave-ston">
+            <p class="memorial">
+            "Thise werk is ded, ful longe tyme forsaken<br>
+            thise page lith as a grave-ston<br>
+            wherof to fewe it stirreth mynde medled<br>
+            of thinge that ne mighte nevere han continuyng."
+            </p>
+        </div>
+        <div>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Know more</a>
+        </div>
     </body>
 </html>

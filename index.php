a92bf3b7b0fe93e4d474fd7aacd97fbb89427948
Ian <ianmarcel@mailfence.com>
2024-12-07 00:11:06 -0300

    00:11:06 - 07/12/2024

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚰️</text></svg>">
-        <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+        <link rel="stylesheet" href="farewell/style.css?v=<?php echo time(); ?>">
         <title>Farewell</title>
     </head>
     <body>
         <div>
-            <img class="grave_ston" src="css/assets/images/ston.png" alt="Grave-ston">
+            <img class="grave_ston" src="farewell/assets/images/ston.png" alt="Grave-ston">
             <p class="memorial">
             "Thise werk is ded, ful longe tyme forsaken<br>
             thise page lith as a grave-ston<br>

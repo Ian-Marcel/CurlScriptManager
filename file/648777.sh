d2c58e548f01c1dc819df791a73ac615e587b814
Ian <ianmarcel@mailfence.com>
2024-12-11 19:31:20 -0300

    19:31:20 - 11/12/2024

--- file/648777.sh
+++ file/648777.sh
@@ -31,11 +31,11 @@ NOTE: reinstall only backup your NGINX configuration folder if it is located in 
 default location${BCYAN}(aka.:/etc/nginx)${BYELLOW}!!!
 ${NC}"
 
-
+while true; do
     read -rp $'Please enter an option using it respective number or stop the wizard by entering 0(zero) \033[1;33m[0,1,2,3]: \033[1;0m' CURL_ESX &&
         if [[ "$CURL_ESX" = 1 ]]; then
             source install.sh
-            #break
+            break
         elif [[ "$CURL_ESX" = 2 ]]; then
             tar -zcf nginx-backup.tar.gz -C /etc/ nginx &&
                 source uninstall.sh &&
@@ -44,10 +44,10 @@ ${NC}"
                 tar -zxf nginx-backup.tar.gz &&
                 mv nginx /etc/ &&
                 rm nginx-backup.tar.gz
-            #break
+            break
         elif [[ "$CURL_ESX" = 3 ]]; then
             source uninstall.sh
-            #break
+            break
         elif [[ "$CURL_ESX" = 0 ]]; then
             echo -e "\n${BGREEN}The wizard was stopped, Bye Bye... \n ${NC}"
             cd .. && rm -rf Easy-Sourced-NGINX easy-sourced-nginx.sh
@@ -55,12 +55,6 @@ ${NC}"
         else
             echo -e "${BYELLOW}Please enter between 0 and 3. ${NC}"
         fi
-
-
-if [ -t 0 ]; then
-    echo "Interactive mode detected"
-else
-    echo "Non-interactive mode detected"
-fi
+done
 
 cd .. && rm -rf Easy-Sourced-NGINX easy-sourced-nginx.sh
